c4735388b68afa60eacb0d3e59c14064082b5fc6
Ryan <rysuzara@gmail.com>
2024-07-11 12:09:48 -0700

    Added Joi validation

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate')
 const Business = require('./models/business')
+const Joi = require('joi')
 const axios = require('axios')
+const catchAsync = require('./utilities/catchAsync')
+const ExpressError = require('./utilities/expressErrors')
 
 
 mongoose.connect('mongodb://localhost:27017/spotless')
@@ -28,42 +31,59 @@ app.use(methodOverride('_method'))
 app.get('/home', (req, res) =>{
     res.render('home')
 })
-app.get('/restrooms', async (req, res) =>{
+app.get('/restrooms', catchAsync(async (req, res, next) =>{
     const restrooms = await Business.find({});
     res.render('restrooms/index', { restrooms })
-})
+}))
 
 app.get('/restrooms/new', (req, res)=>{
     res.render('restrooms/new')
 })
 
-app.post('/restrooms', async(req, res)=> { //logic to create a new bathroom
+app.post('/restrooms', catchAsync( async(req, res, next)=> { //logic to create a new bathroom
+    // if(!req.body.restroom) throw new ExpressError('Invalid data submitted')
+    const restroomSchema = Joi.object({
+        restroom: Joi.object({
+            name: Joi.string().required(),
+            cleanlinessRating:Joi.number().integer().min(1).max(4).required(),
+            location: Joi.string().required(),
+            imageURL: Joi.string().required()
+        }).required()
+
+    })
+    const { error } = restroomSchema.validate(req.body)
+
+    if (error){
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    }
+    console.log(result)
     const restroom = new Business(req.body.restroom);
     await restroom.save();
     res.redirect(`/restrooms/${restroom._id}`)
-})
+}))
 
-app.get('/restrooms/:id', async(req, res,) => {
+app.get('/restrooms/:id', catchAsync (async(req, res, next) => {
     const restroom = await Business.findById(req.params.id)
     res.render('restrooms/show', { restroom })
-})
+}))
 
-app.get('/restrooms/:id/edit', async(req, res,) => {
+app.get('/restrooms/:id/edit', catchAsync (async(req, res, next) => {
     const restroom = await Business.findById(req.params.id)
     res.render('restrooms/edit', { restroom })
-})
+}))
 
-app.put('/restrooms/:id', async(req, res)=> {
+app.put('/restrooms/:id',catchAsync (async(req, res, next)=> {
     const { id } = req.params
     const restroom = await Business.findByIdAndUpdate(id, {...req.body.restroom})
     res.redirect(`/restrooms/${restroom._id}`)
-})
+}))
 
-app.delete('/restrooms/:id', async(req, res) => {
+app.delete('/restrooms/:id', catchAsync (async(req, res, next) => {
     const { id } = req.params;
     await Business.findByIdAndDelete(id);
     res.redirect('/restrooms')
-})
+}))
 
 // app.get('/addBusiness', async (req, res) =>{
 //     const business = new Business({
@@ -77,6 +97,17 @@ app.delete('/restrooms/:id', async(req, res) => {
 // res.send(business)
 // })
 
+app.all('*', (req, res, next)=>{
+    next(new ExpressError('Page not found!', 404))
+})
+
+
+app.use((err, req, res, next) =>{
+    const { statusCode = 500, message = 'Something went wrong' } = err
+    if (!err.message) err.message = 'Oh no! Something went wrong!'
+    res.status(statusCode).render('error', { err })
+})
+
 app.listen(3000, ()=> {
-    console.log('Servinging on port 3000')
+    console.log('Serving on port 3000')
 })
